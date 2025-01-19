f7d3fc40f3af66de07dc49e0c6f3878fa6166891
fakhreddine <largofakhrddin@gmail.com>
2025-01-20 00:00:16 +0100

    initial

---
+++ main.js
@@ -0,0 +1,7 @@
+const btn1p = document.querySelector('.btn1')
+const btn2p = document.querySelector('.btn2')
+
+function btn1(){
+    btn1p.classList.add('hidden')
+    btn2p.classList.remove('hidden')
+}
